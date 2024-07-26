10fce64f78c53c78952b54ec4299f76115f09145
name <name@example.com>
2024-07-26 11:16:51 +0000

    finished: add pushexpress background notification handler

--- notificationsvc/NotificationService.swift
+++ notificationsvc/NotificationService.swift
@@ -5,22 +5,17 @@
 //
 
 import UserNotifications
+import SdkPushExpress
 
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
+    let notificationServiceManager = NotificationManager()
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
+        notificationServiceManager.handleNotification(request: request, contentHandler: contentHandler)
     }
     
     override func serviceExtensionTimeWillExpire() {
