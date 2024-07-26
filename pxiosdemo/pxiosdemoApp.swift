0b14966374238869459fb39e0fb0f3f7022a419b
name <name@example.com>
2024-07-26 10:38:53 +0000

    initial

---
+++ pxiosdemo/pxiosdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pxiosdemoApp.swift
+//  pxiosdemo
+//
+//  Created by I P on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct pxiosdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
